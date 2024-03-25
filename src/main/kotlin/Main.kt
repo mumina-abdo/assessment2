9a253df64fe030953e7fc1ace42a1f650b12238f
mumina-abdo <muminaboruabdo@gmail.com>
2024-03-25 08:35:11 +0300

    adda files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+fun main(){
+    println(Statistics.size)
+    println(Statistics.count())
+    println(Statistics.average())
+
+
+    println(mysent("farbie","fakes","frown","and funs"))
+
+}
+fun myNum = Array<Int>()
+    val Statistics = arrayOf(21, 14, 56, 18, 9, 5, 32, 67, 88, 43)
+
+
+
+fun mysent(a:String,b:String,c:String,d:String):String{
+    var word = "Barnie bakes brown bagels and buns"
+    println("b","f")
+}
+
+
+fun word(name:String):Boolean{
+    var item = "madam,wow,kayak"
+
+
+}
